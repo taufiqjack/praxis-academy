e8e48480ad27066a7fa915cbdb33a9652e151e01
Taufiq Dwi Cahyono <taufiqdwic@gmail.com>
2021-02-08 15:38:48 +0700

    latihan javablock

---
+++ novice/01-01/Latihan/JavaBlock.java
@@ -0,0 +1,10 @@
+public class JavaBlock {
+    public static void main(String[] args) {
+        String band ="Beatlees";
+
+        if (band == "Beatles") {
+            System.out.println("Heu ");
+            System.out.println("Jude! ");
+    }
+    }
+}
