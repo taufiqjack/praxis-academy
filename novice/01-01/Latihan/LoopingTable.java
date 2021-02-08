e94f216b0652f222633849c50e496ab705da6fd1
Taufiq Dwi Cahyono <taufiqdwic@gmail.com>
2021-02-08 15:33:56 +0700

    latihan looping

---
+++ novice/01-01/Latihan/LoopingTable.java
@@ -0,0 +1,51 @@
+public class LoopingTable {
+
+    public static void main(String[] args) {
+        System.out.println("Looping tabel : ");
+        for(int i =1;i<= 10;i++){
+            System.out.println(i + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *2) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *3) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *4) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *5) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *5) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *6) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *7) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *8) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *9) + " ");
+        }
+        System.out.println();
+        for(int i =1;i<= 10;i++){
+            System.out.println((i *10) + " ");
+        }
+        System.out.println();
+    }
+}
+   
