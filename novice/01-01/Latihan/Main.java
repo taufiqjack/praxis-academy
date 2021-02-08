44cc8e909317fdf6b670f9fec760b61ed2f4375e
Taufiq Dwi Cahyono <taufiqdwic@gmail.com>
2021-02-08 15:38:58 +0700

    update main

--- novice/01-01/Latihan/Main.java
+++ novice/01-01/Latihan/Main.java
@@ -1,7 +1,32 @@
 public class Main{
     
-    public static void main(String[]args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
+
+        //desimlasatuan
+        byte a = 5;
+        int b;
+        long c;
+
+        //titikkoma
+        float d = 1.5f;
+        double e = 1.5;
+
+        //karakter
+        char f ='B';
+
+        String g = "Hello Wordl!";
+
+        System.out.println(a);
     }
+
+    // public static void main(String[] args) {
+
+    //     percabangan
+    //     for( int i=0 ;i < 20 ; i++){
+    //         System.out.println(i);
+    //     }
+    // }
 }
+
