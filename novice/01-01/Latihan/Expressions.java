3ad44a5bf3cac330d64dae9eb0d2b6bbd9bd6c20
Taufiq Dwi Cahyono <taufiqdwic@gmail.com>
2021-02-08 15:37:28 +0700

    latihan expresion

---
+++ novice/01-01/Latihan/Expressions.java
@@ -0,0 +1,13 @@
+import javax.naming.spi.DirStateFactory.Result;
+
+public class Expressions {
+    int score;
+    score =90;
+
+    double a =2.2 , b=3.4, result;
+    result = a +b - 3.4;
+
+    if (number1 == number 2){
+        System.out.println("Number 1 is larger than number 2");
+    }
+}
