ddbd7fe8a7bf42a7f41ee9e14971862f955501b7
Taufiq Dwi Cahyono <taufiqdwic@gmail.com>
2021-02-08 16:36:27 +0700

    kasus matrik nomor 1

---
+++ novice/01-01/kasus/MatriksArrayDim.java
@@ -0,0 +1,16 @@
+public class MatriksArrayDim {
+    public static void main(String[] args) {
+        int A[][] = {{1,2,3},{4,5,6}};
+        int B[][] = {{2,2,2},{2,2,2}};
+        int C[][] = new int[3][3];
+        
+        for(int i = 0;i<2;i++)
+        {
+            for(int j=0;0<2;j++)
+            {
+                System.out.println(C[i][j]=A[i][j]+B[i][j]);
+            }
+        }
+    
+    }
+}
