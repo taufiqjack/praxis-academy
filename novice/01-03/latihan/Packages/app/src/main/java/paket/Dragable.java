1109bb95b38b362c23f49c62da0fa17524a3c7e1
taufiqjack <taufiqdwic@gmail.com>
2021-02-10 13:31:00 +0700

    membuat packages

---
+++ novice/01-03/latihan/Packages/app/src/main/java/paket/Dragable.java
@@ -0,0 +1,48 @@
+package paket;
+
+// package graphics;
+public interface Dragable {
+
+}
+
+/**
+ * Graphic
+ */
+// package graphics;
+public abstract class Graphic {
+
+}
+
+/**
+ * Circle
+ */
+// package graphics;
+public class Circle extends Graphic implements Dragable{
+
+}
+
+/**
+ * Rectangle
+ */
+// package graphics;
+public class Rectangle extends Graphic implements Dragable  {
+
+    
+}
+
+/**
+ * Point
+ */
+// package graphics;
+public class Point extends Graphic implements Dragable {
+
+    
+}
+/**
+ * Line
+ */
+// package graphics;
+public class Line extends Graphic implements Dragable{
+
+    
+}
