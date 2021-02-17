742b4a80a5e743e22972523e2176d4d95d8b87bf
taufiqjack <taufiqdwic@gmail.com>
2021-02-17 10:19:30 +0700

    membuat kasus pattern

--- novice/02-01/kasus/app/src/main/java/kasusuji/Bicycle.java
+++ novice/02-01/kasus/app/src/main/java/kasusuji/Bicycle.java
@@ -11,26 +11,25 @@ public abstract class Bicycle {
         this.speed = speed;
         this.gear = gear;
     }
-    
 
-    // public static void main(String[] args) {
-    // BicycleUpgrade bike1 = new BicycleUpgrade();
-    // BicycleUpgrade bike2 = new BicycleUpgrade();
+    public static void main(String[] args) {
+        BicycleUpgrade bike1 = new BicycleUpgrade();
+        BicycleUpgrade bike2 = new BicycleUpgrade();
 
-    // bike1.changeCadence(50);
-    // bike1.speedUp(10);
-    // bike1.changeGear(2);
-    // bike1.printStates();
+        bike1.changeCadence(50);
+        bike1.speedUp(10);
+        bike1.changeGear(2);
+        bike1.printStates();
 
-    // bike2.changeCadence(50);
-    // bike2.speedUp(10);
-    // bike2.changeGear(2);
-    // bike2.changeCadence(40);
-    // bike2.speedUp(10);
-    // bike2.changeGear(3);
-    // bike2.printStates();
-    // ;
+        bike2.changeCadence(50);
+        bike2.speedUp(10);
+        bike2.changeGear(2);
+        bike2.changeCadence(40);
+        bike2.speedUp(10);
+        bike2.changeGear(3);
+        bike2.printStates();
+        ;
 
-    // }
+    }
 
 }
